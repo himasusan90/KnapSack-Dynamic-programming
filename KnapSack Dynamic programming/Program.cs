3910998a0cf1f0835a1b4c3c750f65041da4bd41
hima.jose@mintgroup.net <hima.jose@mintgroup.net>
2023-11-30 16:41:17 +0530

    Add project files.

---
+++ KnapSack Dynamic programming/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace KnapSack_Dynamic_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxcapacity = 10;
+            const int n = 4;
+            int[] weights = { 0, 8, 2, 6, 1 };
+            int[] values = { 0, 50, 150, 210, 30 };
+            int[,] data = new int[5, 11];
+            string[] itemNames = { "Microwave", "Drone", "Monitor", "kettle" };
+
+            GetMaxValueInContainer(maxcapacity, n, weights, values, data);
+            Console.WriteLine($"Max value of items included in container:{data[n, maxcapacity]}");
+            OutputItemInclusionStatus(n, maxcapacity, weights, values, itemNames, data);
+        }
+
+        private static void OutputItemInclusionStatus(int n, int maxcapacity, int[] weights, int[] values, string[] itemNames, int[,] data)
+        {
+            int i = n;
+            int j = maxcapacity;
+            while(i>0 && j > 0)
+            {
+                if (data[i, j] == data[i - 1, j]){
+                    Console.WriteLine("item "+itemNames[i-1]+"not included in the knapsack" );
+                }
+                else
+                {
+                    Console.WriteLine("item " + itemNames[i-1] + " is included in the knapsack");
+                    j = j - weights[i];
+                }
+                i--;
+            }
+        }
+
+        private static void GetMaxValueInContainer(int maxcapacity, int n, int[] weights, int[] values, int[,] data)
+        {
+            for (int itemNum = 0; itemNum <= n; itemNum++)
+            {
+                for (int capacity = 0; capacity <= maxcapacity; capacity++)
+                {
+                    if (itemNum == 0 || capacity == 0)
+                    {
+                        data[itemNum, capacity] = 0;
+                    }
+                    else if (weights[itemNum] <= capacity)
+                    {
+                        data[itemNum, capacity] =
+                             Math.Max(data[itemNum - 1, capacity],
+                             values[itemNum] + data[itemNum - 1, capacity - weights[itemNum]]);
+                    }
+                    else
+                    {
+                        data[itemNum, capacity] = data[itemNum - 1, capacity];
+                    }
+                }
+            }
+        }
+    }
+
+  
+}
